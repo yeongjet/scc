d1596821df69dae648d1e1b29865da9352fab058
yeongjet <yeongjet@gmail.com>
2024-06-30 18:20:07 +0800

    update debug configurations

---
+++ test/a.c
@@ -0,0 +1,29 @@
+/***********************************************************
+ * HelloWorld.cԴ�ļ�
+ **********************************************************/
+
+int x(int a) {
+    return a+1;
+}
+/*
+int y(int a) {
+    return a+1;
+}
+*/
+int main()
+{/*
+   int a = x(2);
+   
+    //int b = y(3);
+    //int c = a + b;
+    //printf("he:%d", a);*/
+    printf("sdfsdf:%d", 1);
+    return 0;
+}
+
+void _entry()
+{
+    int ret;
+    ret = main();
+    exit(ret);
+}
